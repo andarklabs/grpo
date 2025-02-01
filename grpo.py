03c9d5ecb73d6b450b95621ecc8b8daa5fa49f67
aceniccola <andrewsamce@me.com>
2025-02-01 15:36:19 -0500

    grpo outlined

--- grpo.py
+++ grpo.py
@@ -3,17 +3,23 @@ import kl_div as kl
 
 EPSILON = 0
 BETA = 0
+G = 6
 
-def main():
-    print("Hello from grpo!")
-    return 
+def main(o_i, p_r, p_o, p, a, b = BETA, g = G):
+    for o_i in range(g):
+        a = advantage(o_i)
+        return np.sum(np.sum(min(p,p_o,a)-b*kl.unbiased_estimator_kl_div(p_r,p))/len(o_i))/g
+    # print("Hello from grpo!")
 
 
-def min(p,p_o,a):
-    return np.min(p/p_o, np.clip(p/p_o,1-EPSILON,1+EPSILON))*a
-
-def cycle():
+def advantage(o_i):
     pass
+
+def min(p,p_o,a, e = EPSILON):
+    return np.min(p/p_o, np.clip(p/p_o,1-e,1+e))*a
+
+'''def cycle(o_i, p_r, p_o, p, a, b = BETA):
+    return np.sum(min(p,p_o,a)-b*kl.unbiased_estimator_kl_div(p_r,p))/len(o_i)'''
 
 if __name__ == "__main__":
     main()
