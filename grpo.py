34c37de67d16332e46eb4817712cdd5e1b5a4ce5
aceniccola <andrewsamce@me.com>
2025-02-01 15:41:08 -0500

    grpo

--- grpo.py
+++ grpo.py
@@ -1,13 +1,15 @@
 import numpy as np
 import kl_div as kl
+import advantage as adv
 
 EPSILON = 0
 BETA = 0
 G = 6
 
-def main(o_i, p_r, p_o, p, a, b = BETA, g = G):
+def grpo(o_i, p_r, p_o, p, a,r_i,r, b = BETA, g = G):
     for o_i in range(g):
-        a = advantage(o_i)
+        # TODO: calc reward on o_i
+        a = adv(r_i, r)
         return np.sum(np.sum(min(p,p_o,a)-b*kl.unbiased_estimator_kl_div(p_r,p))/len(o_i))/g
     # print("Hello from grpo!")
 
@@ -18,8 +20,10 @@ def advantage(o_i):
 def min(p,p_o,a, e = EPSILON):
     return np.min(p/p_o, np.clip(p/p_o,1-e,1+e))*a
 
-'''def cycle(o_i, p_r, p_o, p, a, b = BETA):
-    return np.sum(min(p,p_o,a)-b*kl.unbiased_estimator_kl_div(p_r,p))/len(o_i)'''
+'''
+def cycle(o_i, p_r, p_o, p, a, b = BETA):
+    return np.sum(min(p,p_o,a)-b*kl.unbiased_estimator_kl_div(p_r,p))/len(o_i)
+'''
 
 if __name__ == "__main__":
-    main()
+    grpo()
